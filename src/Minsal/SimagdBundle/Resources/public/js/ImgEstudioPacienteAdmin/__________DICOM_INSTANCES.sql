d1a37b38887cd1f1cf63bc912e55f3f9cb985546
Farid Hernandez Cortez <farid.hdz.64@gmail.com>
2016-07-07 04:53:00 -0600

    yeah baby

---
+++ src/Minsal/SimagdBundle/Resources/public/js/ImgEstudioPacienteAdmin/__________DICOM_INSTANCES.sql
@@ -0,0 +1,166 @@
+http://10.10.56.5:8081/wado?requestType=WADO&studyUID=1.2.392.200036.9125.2.112981841818461.64811355338.5256796&seriesUID=1.2.392.200036.9125.3.112981841818461.64811355338.5256798&objectUID=1.2.392.200036.9125.9.0.3826709404.1174461580.869593287&frameNumber=1&contentType=image/png
+
+Specific Character Set	(0008,0005)	CS	10	ISO_IR 100
+Image Type	(0008,0008)	CS	44	DERIVED\PRIMARY\POST_PROCESSED\RT\\\\\150000
+SOP Class UID	(0008,0016)	UI	26	1.2.840.10008.5.1.4.1.1.1
+SOP Instance UID	(0008,0018)	UI	56	1.2.392.200036.9125.9.0.3826709404.1174461580.869593287
+Acquisition Date	(0008,0022)	DA	8	20160524
+Content Date	(0008,0023)	DA	8	20160524
+Acquisition Time	(0008,0032)	TM	10	101619.026
+Content Time	(0008,0033)	TM	10	101619.198
+Retrieve AE Title	(0008,0054)	AE	8	DCM4CHEE
+Instance Availability	(0008,0056)	CS	6	ONLINE
+Instance Number	(0020,0013)	IS	4	1001
+Photometric Interpretation	(0028,0004)	CS	12	MONOCHROME1
+Rows	(0028,0010)	US	2	2304
+Columns	(0028,0011)	US	2	2880
+Bits Allocated	(0028,0100)	US	2	16
+
+
+
+
+http://10.10.56.5:8081/wado?requestType=WADO&studyUID=1.2.392.200036.9125.2.112981841818461.64811355338.5256796&seriesUID=1.2.392.200036.9125.3.112981841818461.64811355456.5356298&objectUID=1.2.392.200036.9125.9.0.3826667933.1208016012.869593287&frameNumber=1&contentType=image/png
+
+Specific Character Set	(0008,0005)	CS	10	ISO_IR 100
+Image Type	(0008,0008)	CS	44	DERIVED\PRIMARY\POST_PROCESSED\RT\\\\\150000
+SOP Class UID	(0008,0016)	UI	26	1.2.840.10008.5.1.4.1.1.1
+SOP Instance UID	(0008,0018)	UI	56	1.2.392.200036.9125.9.0.3826667933.1208016012.869593287
+Acquisition Date	(0008,0022)	DA	8	20160524
+Content Date	(0008,0023)	DA	8	20160524
+Acquisition Time	(0008,0032)	TM	10	101753.221
+Content Time	(0008,0033)	TM	10	101753.420
+Retrieve AE Title	(0008,0054)	AE	8	DCM4CHEE
+Instance Availability	(0008,0056)	CS	6	ONLINE
+Instance Number	(0020,0013)	IS	4	1003
+Photometric Interpretation	(0028,0004)	CS	12	MONOCHROME1
+Rows	(0028,0010)	US	2	2880
+Columns	(0028,0011)	US	2	2304
+Bits Allocated	(0028,0100)	US	2	16
+
+
+
+
+http://10.10.56.5:8081/wado?requestType=WADO&studyUID=2.16.840.1.113669.632.20.1211.10001373596&seriesUID=1.3.12.2.1107.5.1.4.1003.30000011040706253106200004245&objectUID=1.3.12.2.1107.5.1.4.1003.30000011040706253106200004545&frameNumber=1&contentType=image/png
+
+Study Instance UID	(0020,000D)	UI	42	2.16.840.1.113669.632.20.1211.10001373596
+Study ID	(0020,0010)	SH	12	A10073945674
+Series Instance UID	(0020,000E)	UI	54	1.3.12.2.1107.5.1.4.1003.30000011040706253106200004245
+Specific Character Set	(0008,0005)	CS	10	ISO_IR 100
+Image Type	(0008,0008)	CS	34	DERIVED\PRIMARY\AXIAL\CT_SOM5 SPI
+SOP Class UID	(0008,0016)	UI	26	1.2.840.10008.5.1.4.1.1.2
+SOP Instance UID	(0008,0018)	UI	54	1.3.12.2.1107.5.1.4.1003.30000011040706253106200004545
+Acquisition Date	(0008,0022)	DA	8	20110407
+Content Date	(0008,0023)	DA	8	20110407
+Acquisition Time	(0008,0032)	TM	14	153912.850324
+Content Time	(0008,0033)	TM	14	153912.850324
+Retrieve AE Title	(0008,0054)	AE	8	DCM4CHEE
+Instance Availability	(0008,0056)	CS	6	ONLINE
+Instance Number	(0020,0013)	IS	4	300
+Photometric Interpretation	(0028,0004)	CS	12	MONOCHROME2
+Rows	(0028,0010)	US	2	512
+Columns	(0028,0011)	US	2	512
+Bits Allocated	(0028,0100)	US	2	16
+
+
+
+
+http://10.10.56.5:8081/wado?requestType=WADO&studyUID=2.16.840.1.113669.632.20.1211.10001373596&seriesUID=1.3.12.2.1107.5.1.4.1003.30000011040706253106200004245&objectUID=1.3.12.2.1107.5.1.4.1003.30000011040706253106200004829&frameNumber=1&contentType=image/png
+
+Study Instance UID	(0020,000D)	UI	42	2.16.840.1.113669.632.20.1211.10001373596
+Study ID	(0020,0010)	SH	12	A10073945674
+Series Instance UID	(0020,000E)	UI	54	1.3.12.2.1107.5.1.4.1003.30000011040706253106200004245
+Series Number	(0020,0011)	IS	2	3
+Specific Character Set	(0008,0005)	CS	10	ISO_IR 100
+Image Type	(0008,0008)	CS	34	DERIVED\PRIMARY\AXIAL\CT_SOM5 SPI
+SOP Class UID	(0008,0016)	UI	26	1.2.840.10008.5.1.4.1.1.2
+SOP Instance UID	(0008,0018)	UI	54	1.3.12.2.1107.5.1.4.1003.30000011040706253106200004829
+Acquisition Date	(0008,0022)	DA	8	20110407
+Content Date	(0008,0023)	DA	8	20110407
+Acquisition Time	(0008,0032)	TM	14	153922.097146
+Content Time	(0008,0033)	TM	14	153922.097146
+Retrieve AE Title	(0008,0054)	AE	8	DCM4CHEE
+Instance Availability	(0008,0056)	CS	6	ONLINE
+Instance Number	(0020,0013)	IS	4	584
+Photometric Interpretation	(0028,0004)	CS	12	MONOCHROME2
+Rows	(0028,0010)	US	2	512
+Columns	(0028,0011)	US	2	512
+Bits Allocated	(0028,0100)	US	2	16
+
+
+
+
+http://10.10.56.5:8081/wado?requestType=WADO&studyUID=2.16.840.1.113669.632.20.1211.10001373596&seriesUID=1.3.12.2.1107.5.1.4.1003.30000011040706264242100002270&objectUID=1.3.12.2.1107.5.1.4.1003.30000011040706264242100002284&frameNumber=1&columns=168&contentType=image/png
+
+Study Instance UID	(0020,000D)	UI	42	2.16.840.1.113669.632.20.1211.10001373596
+Study ID	(0020,0010)	SH	12	A10073945674
+Series Instance UID	(0020,000E)	UI	54	1.3.12.2.1107.5.1.4.1003.30000011040706264242100002270
+Specific Character Set	(0008,0005)	CS	10	ISO_IR 100
+Image Type	(0008,0008)	CS	16	ORIGINAL\PRIMARY
+SOP Class UID	(0008,0016)	UI	28	1.2.840.10008.5.1.4.1.1.128
+SOP Instance UID	(0008,0018)	UI	54	1.3.12.2.1107.5.1.4.1003.30000011040706264242100002284
+Acquisition Date	(0008,0022)	DA	8	20110407
+Content Date	(0008,0023)	DA	8	20110407
+Acquisition Time	(0008,0032)	TM	14	153944.968000
+Content Time	(0008,0033)	TM	14	161318.000000
+Retrieve AE Title	(0008,0054)	AE	8	DCM4CHEE
+Instance Availability	(0008,0056)	CS	6	ONLINE
+Instance Number	(0020,0013)	IS	2	14
+Photometric Interpretation	(0028,0004)	CS	12	MONOCHROME2
+Rows	(0028,0010)	US	2	168
+Columns	(0028,0011)	US	2	168
+Bits Allocated	(0028,0100)	US	2	16
+
+
+
+
+http://10.10.56.5:8081/wado?requestType=WADO&studyUID=2.16.840.1.113669.632.20.1211.10001373596&seriesUID=1.3.12.2.1107.5.1.4.1003.30000011040706253106200004245&objectUID=1.3.12.2.1107.5.1.4.1003.30000011040706253106200004274&frameNumber=1&contentType=image/png
+
+/*
+ * study query for demo
+ */
+select	
+	u.study_iuid,
+	u.study_id,
+	u.study_datetime,
+	u.accession_no,
+	u.study_desc,
+	u.num_series,
+	u.num_instances,
+	
+	s.series_iuid,
+	s.series_no,
+	s.modality,
+	s.body_part,
+	s.series_desc,
+	s.institution,
+	s.station_name,
+	s.num_instances,
+	s.src_aet,
+	s.fileset_iuid,
+	
+	i.sop_iuid, 
+	i.inst_no, 
+	--i.inst_desc, 
+	i.created_time, 
+	i.inst_status, 
+	i.retrieve_aets 
+from instance i 
+    inner join series s on s.pk = i.series_fk 
+    inner join study u on u.pk = s.study_fk 
+where sop_iuid like '%1.2.392.200036.9125.9.0.3826667933.1208016012.869593287%' or sop_iuid like '%1.3.12.2.1107.5.1.4.1003.30000011040706253106200004545%';
+
+/*
+ * material query for demo
+ */
+select 
+--regexp_replace(g.codigo, '^\s+', '') as codigo, 
+--regexp_replace(g.nombre, '^\s+', '') as grupo, 
+regexp_replace(s.codigo, '^\s+', '') as codigo, 
+regexp_replace(s.nombre, '^\s+', '') as grupo, 
+regexp_replace(m.codigo, '^\s+', '') as codigo, 
+regexp_replace(m.nombre, '^\s+', '') as grupo 
+from img_ctl_material m 
+    inner join img_ctl_subgrupo_material s on s.id = m.id_subgrupo_material 
+    inner join img_ctl_grupo_material g on g.id = s.id_grupo_material
+where g.nombre like '%RAYO%'
+order by m.id, g.nombre, s.nombre, m.nombre;
